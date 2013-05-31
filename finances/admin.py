9be3e3c756a4fc63451602b5c8eaa4086d5b6c35
Eduardo Rivas <e@jerivas.com>
2013-05-30 23:47:51 -0600

    Finances: Transaction admin and model cleanup.

--- finances/admin.py
+++ finances/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
-from finances.models import Budget, Transaction
+from finances.models import Transaction
 
-admin.site.register(Budget)
-admin.site.register(Transaction)
+
+class TransactionAdmin(admin.ModelAdmin):
+    fields = (("transaction_id", "transaction_type"),
+              ("transaction_date", "amount"),
+              ("origin", "destination"))
+
+    list_display = ("transaction_id", "transaction_type", "amount_as_currency",
+                    "transaction_date", "origin", "destination")
+    list_filter = ("transaction_type",)
+    date_hierarchy = "transaction_date"
+    search_fields = ("transaction_id", "origin", "destination")
+
+    def amount_as_currency(self, model):
+        return "$%s" % model.amount
+    amount_as_currency.short_description = _("Amount")
+    amount_as_currency.admin_order_field = "amount"
+
+admin.site.register(Transaction, TransactionAdmin)
--- finances/models.py
+++ finances/models.py
@@ -1,23 +1,25 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.utils.timezone import now
 
 
 class Transaction(models.Model):
     """A transaction related to a budget."""
-    TYPE_CHOICES = (
+    TRANSACTION_TYPE_CHOICES = (
         ("income", _("Income")),
         ("egress", _("Egress")),
     )
     transaction_id = models.CharField(_("Transaction ID"), max_length=16,
                      blank=False)
     transaction_type = models.CharField(_("Type"), max_length=16, blank=False,
-                       choices=TYPE_CHOICES, default="income")
-    transaction_date = models.DateField(_("Date"), blank=False)
+                       choices=TRANSACTION_TYPE_CHOICES, default="income")
+    transaction_date = models.DateField(_("Date"), blank=True, null=True)
     amount = models.DecimalField(_("Amount"), max_digits=6,
              decimal_places=2, blank=False)
-    origin = models.CharField(_("Origin"), max_length=128, blank=False)
+    origin = models.CharField(_("Origin"), max_length=128, blank=True,
+             null=True)
     destination = models.CharField(_("Destination"), max_length=128,
-                  blank=False)
+                  blank=True, null=True)
 
     def __unicode__(self):
         return "%s - $%s (%s)" % (self.transaction_id, self.amount,
@@ -27,3 +29,9 @@ class Transaction(models.Model):
         ordering = ["-transaction_date"]
         verbose_name = _("Transaction")
         verbose_name_plural = _("Transactions")
+
+    def save(self, *args, **kwargs):
+        """Set default for ``transaction_date`` as now"""
+        if self.transaction_date is None:
+            self.transaction_date = now()
+        super(Transaction, self).save(*args, **kwargs)
