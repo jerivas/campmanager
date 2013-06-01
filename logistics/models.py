49d41b729412524951373a2ecdc875483283385a
Eduardo Rivas <e@jerivas.com>
2013-05-31 23:24:44 -0600

    Logistics: Catch exception when saving Small Groups with no Counselor. This happens when creating the Small Groups and assigning the Counselor later.

--- logistics/models.py
+++ logistics/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from logistics.choices import GENERATION_MATCHING
-
 
 class SmallGroup(models.Model):
     """A small group composed of several Campers and a Counselor"""
@@ -28,12 +26,25 @@ class SmallGroup(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
+        """
+        This method matches the structure to the selected generation via
+        the GENERATION_MATCHING tuple. It then saves the object and proceeds
+        to update all related objects (Campers and Counselor). An Exception is
+        catched if the Counselor hasn't been assigned yet (because the
+        Small Group might be new, for example).
+        """
+        from logistics.choices import GENERATION_MATCHING
+        from signup.models import Counselor
+
         for structure in GENERATION_MATCHING:
             if self.generation in structure[1]:
                 self.structure = structure[0]
         super(SmallGroup, self).save(*args, **kwargs)
-        self.counselor.save(update_fields=["structure", "generation",
-            "cabin", "bus"])
+        try:
+            self.counselor.save(update_fields=["structure", "generation",
+                "cabin", "bus"])
+        except Counselor.DoesNotExist:
+            pass
         self.camper_set.update(structure=self.structure,
             generation=self.generation, cabin=self.cabin, bus=self.bus)
 
