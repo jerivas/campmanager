c4ac65a0bc7906b8430700d159cfd8a5af906d6c
Eduardo Rivas <e@jerivas.com>
2014-05-29 15:52:38 -0600

    General: New app and management command for performing yearly DB resets.
    python manage.py newcamp will reset the database without destroying
    camper records, ideal to start a new camp without losing camper entries.

---
+++ general/management/commands/newcamp.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Deletes all financial records, graduates all generations, " \
+           "and resets all customs permissions. Camper records are kept" \
+           "for the convinience of not having to enter them again."
+
+    def handle(self, *args, **options):
+        from logistics.choices import GENERATIONS
+        from logistics.models import SmallGroup
+        from signup.models import Payment, Guest, Parent
+        from finances.models import Transaction
+
+        self.stdout.write("Deleting financial records")
+        Payment.objects.all().delete()
+        Transaction.objects.all().delete()
+        self.stdout.write("All financial records deleted")
+
+        self.stdout.write("Deleting Guests")
+        Guest.objects.all().delete()
+        self.stdout.write("All Guests deleted")
+
+        self.stdout.write("Graduating generations")
+        last_generation = GENERATIONS[-1][0]
+        for group in SmallGroup.objects.all():
+            if group.generation == last_generation:
+                group.delete()
+            else:
+                group.generation += 1
+                group.save()
+                group.camper_set.update(balance=0, no_pay=False,
+                                        permission_status=0)
+                group.counselor.balance = 0
+                group.counselor.no_pay = False
+                group.counselor.save()
+        self.stdout.write("All generations graduated successfully")
+
+        self.stdout.write("Deleting Parents without Children")
+        Parent.objects.filter(mothered=None, fathered=None).delete()
+        self.stdout.write("All left-over Parents deleted")
