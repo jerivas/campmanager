bbaa4fbe715797d00b36326bacec3f09bbddb0bc
Eduardo Rivas <e@jerivas.com>
2015-05-31 19:02:58 -0600

    Signup: Add migration for permission_status changes.

---
+++ signup/migrations/0007_auto_20150531_1902.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('signup', '0006_auto_20150524_1732'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='camper',
+            name='permission_status',
+            field=models.IntegerField(default=0, verbose_name='Permission Status', choices=[(0, 'Pending Documentation'), (1, 'Ready to Print'), (2, 'Printed'), (3, 'Signed'), (4, 'Proofread'), (5, 'Special Case')]),
+        ),
+    ]
