d44898e3f1e843555298dd66449b1aa82ba1355b
Eduardo Rivas <e@jerivas.com>
2015-06-20 16:08:45 -0600

    General: Enable the request context processor.

--- campmanager/settings.py
+++ campmanager/settings.py
@@ -51,6 +51,31 @@ TEMPLATE_LOADERS = (
     "django.template.loaders.app_directories.Loader",
 )
 
+# New template settings introduced in Django 1.8
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            # insert your TEMPLATE_DIRS here
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                # Defaults
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+                # Custom
+                "django.template.context_processors.request",
+            ],
+        },
+    },
+]
+
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
