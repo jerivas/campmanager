8462d4d966a3cd237e96eb619e5f2b44a90c1717
Eduardo Rivas <e@jerivas.com>
2016-05-15 21:25:00 -0600

    General: Enable all of Django's auth views to support password reset.

--- campmanager/urls.py
+++ campmanager/urls.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
 
 from .views import Home
 
@@ -11,9 +11,16 @@ urlpatterns = [
     # Grappelli admin skin
     url(r"^grappelli/", include("grappelli.urls")),
 
-    # Login page with redirection capabilities via "next" param
+    # Override the login view to use the admin template
     url(r"^accounts/login/$",
-        login, {"template_name": "admin/login.html"}, name="login"),
+        auth_views.login, {"template_name": "admin/login.html"}, name="login"),
+    # Grappelli looks for a pattern named "admin_password_reset" to display the
+    # "Forgot your password?" link in the login form
+    url(r"^accounts/password_reset/$",
+        auth_views.password_reset, name="admin_password_reset"),
+
+    # Include the rest of the auth URLs normally
+    url(r"^accounts/", include("django.contrib.auth.urls")),
 
     # Uncomment the next line to enable the admin:
     url(r"^staff/", include(admin.site.urls)),
