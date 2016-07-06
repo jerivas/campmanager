5c534e98a3f4c724809e5920298f0786e3df7741
Eduardo Rivas <e@jerivas.com>
2016-07-05 22:43:04 -0600

    Signu: Add deduplicate management command.
    This will list potential duplicated Campers, but won't delete them.

---
+++ signup/management/commands/deduplicate.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Attempts to find duplicates in signup records."
+
+    def handle(self, *args, **kwargs):
+        from ...models import Camper, Counselor
+
+        campers = Camper.objects.filter(signed_up=True)
+
+        for camper in campers:
+            # Check for similar Campers
+            filters = {
+                "first_name__unaccent__icontains": camper.first_name,
+                "first_surname__unaccent__icontains": camper.first_surname,
+            }
+            if campers.filter(**filters).exclude(pk=camper.pk):
+                self.stdout.write("Duplicate camper: %s" % camper)
+
+            # Check for Campers added as Counselors
+            filters.update({"small_group_id": camper.small_group_id})
+            if Counselor.objects.filter(**filters):
+                self.stdout.write("Counselor added as camper: %s" % camper)
