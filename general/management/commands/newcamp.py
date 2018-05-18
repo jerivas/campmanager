72149c4110c05e1a3e449f05eba429c705137084
Eduardo Rivas <e@jerivas.com>
2018-05-17 22:00:20 -0600

    Signup: Don't graduate generations when running newcamp command.

--- general/management/commands/newcamp.py
+++ general/management/commands/newcamp.py
@@ -2,9 +2,9 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Deletes all financial records, graduates all generations, " \
-           "and resets all customs permissions. Camper records are kept" \
-           "for the convinience of not having to enter them again."
+    help = "Deletes all financial records, resets all small groups, " \
+           "and customs information. Camper records are kept" \
+           "for the convenience of not having to enter them again."
 
     def handle(self, *args, **options):
         from logistics.choices import GENERATIONS
@@ -26,32 +26,30 @@ class Command(BaseCommand):
         Guest.objects.all().delete()
         self.stdout.write("All Guests deleted")
 
-        self.stdout.write("Graduating generations")
+        self.stdout.write("Resseting small groups")
         last_generation = GENERATIONS[-1][0]
         for group in SmallGroup.objects.all():
-            if group.generation == last_generation:
-                group.delete()
-            else:
+            if group.generation != last_generation:
                 group.generation += 1
-                group.cabin = group.bus = ""
-                group.save()
-                reset_fields = {
-                    "balance": 0,
-                    "no_pay": False,
-                    "signed_up": False,
-                    "permission_status": Camper.INCOMPLETE,
-                    "fined": False,
-                    "has_medical_record": False,
-                }
-                group.camper_set.update(**reset_fields)
-                for field, value in reset_fields.items():
-                    try:
-                        setattr(group.counselor, field, value)
-                    except AttributeError:
-                        pass
-                group.counselor.has_gov_id = False
-                group.counselor.save()
-        self.stdout.write("All generations graduated successfully")
+            group.cabin = group.bus = ""
+            group.save()
+            reset_fields = {
+                "balance": 0,
+                "no_pay": False,
+                "signed_up": False,
+                "permission_status": Camper.INCOMPLETE,
+                "fined": False,
+                "has_medical_record": False,
+            }
+            group.camper_set.update(**reset_fields)
+            for field, value in reset_fields.items():
+                try:
+                    setattr(group.counselor, field, value)
+                except AttributeError:
+                    pass
+            group.counselor.has_gov_id = False
+            group.counselor.save()
+        self.stdout.write("All small groups have been reset")
 
         self.stdout.write("Deleting Parents without children in record")
         parents = Parent.objects.filter(mothered=None, fathered=None).delete()
